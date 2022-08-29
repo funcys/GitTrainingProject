64c6933f318822f75dfc84e725ab3c799643e86b
funcys <ledhouse2077@gmail.com>
2022-08-29 20:55:59 +0900

    お試し研究ブランチでコミットNo.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 public class Main {
 		public static void main(String[] args) {
 			System.out.println("GITの練習用プロジェクトです");
+
+			aisatsu("こんにちは");
+		}
+
+
+		public static void aisatsu(String word) {
+			String aisatsu = "私の挨拶は" + word;
+			System.out.println(aisatsu);
 		}
 
 }
